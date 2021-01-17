2be1a109a8f31e843a3ccffedf47620c57cd7166
SyedaKhan <77018449+SyedaKhan@users.noreply.github.com>
2021-01-17 19:42:41 +0000

    Add files via upload
    task 1

---
+++ main.py
@@ -0,0 +1,13 @@
+Pizza = input ("Hello, What Pizza would you like?")
+StuffedCrust = input ("Would you like Stuffed Crust with that?")
+if StuffedCrust == "Yes":
+    print("Great, I will add that to your order.")
+else:
+    print("That's fine")
+DeepPan = input ("Would you like want Deep Pan?")
+if DeepPan == "Yes":
+    print("Great, that's added to your order." + " Please proceed to payment.")
+else:
+    print("Thank you for your order, Please proceed to payment.")
+
+
