3b032743fe0a9883bfde4125339c63b5ca1c5b24
SyedaKhan <77018449+SyedaKhan@users.noreply.github.com>
2021-01-18 20:21:19 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-def function_1():
-    value = 2000
-    while value > 1000:
+value = 2000
+def function_1 (value):
+        print("The value of the motorbike is £", "value")
+
+while value >= 1000:
+        function_1
         print("£",value)
-    value = value * 0.9
-return print
+        value=value*0.9
 
 
